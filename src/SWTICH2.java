d4df9f565d4c0ab6f00628b9dbc623f99daff7dd
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-03-13 20:24:32 +0900

    while

---
+++ src/SWTICH2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SWTICH2 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
---
+++ src/whileans.java
@@ -0,0 +1,14 @@
+public class whileans {
+    public static void main(String[] args) {
+
+    int num=1;
+    int sum=0;
+
+    while(num<=10){
+        sum+=num;
+        num++;
+    }
+        System.out.println(sum);
+        System.out.println(num);
+    }
+}
