b929369044c41e78c5902620cc63b2f0c485082f
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-03-13 21:08:12 +0900

    for문

---
+++ src/forex1.java
@@ -0,0 +1,27 @@
+public class forex1 {
+    public static void main(String[] args) {
+
+
+
+        int count=1;
+        int sum=0;
+
+        //초기식 , 조건식  ,, 증감식!
+        for(int i=0;  i<10; i++,count++){
+
+            sum+=count;
+        }
+        System.out.println(sum);
+
+        int num=1;
+        int total=0;
+
+        while(num<=10){
+            total+=num;
+            num++;
+
+        }
+        System.out.println(total);
+    }
+
+}
