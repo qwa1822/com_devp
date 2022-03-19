36351a3b4c097204a7dce6fa044b5f73b3783b52
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-03-20 00:13:48 +0900

    생성자

---
+++ src/java_egoing/IntPrint.java
@@ -0,0 +1,13 @@
+package java_egoing;
+
+public class IntPrint {
+    public static void main(String[] args) {
+
+       int i=0;
+
+       while(i<=100){
+           System.out.println(i);
+           i++;
+       }
+    }
+}
