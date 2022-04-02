13df6fd73b92efd0907c714f52beb55d5dc33e41
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-02 18:36:14 +0900

    variable, printf Test!

---
+++ src/udemy/ex02.java
@@ -0,0 +1,36 @@
+package udemy;
+
+public class ex02 {
+    public static void main(String[] args) {
+
+
+        System.out.printf("5*2=10 %d",5*2);
+        System.out.println();
+        System.out.printf("5*2= %d",5*2);
+        System.out.println();
+        System.out.printf("%d %d %d",5,7,5*7);
+        System.out.println();
+        System.out.printf("%d + %d + %d = %d",5,6,7,5+6+7);
+
+
+        System.out.println();
+        System.out.printf("%s","Testing");
+        System.out.println();
+        System.out.printf("%.1f+%.1f+%.1f",5.5,6.5,7.5);
+        System.out.println();
+        int number=10;
+
+        number=11;
+
+        System.out.println(number);
+
+        int a=5;
+        int b=3;
+        int c=5;
+
+        System.out.printf("%d+%d+%d=%d",a,b,c,a+b+c);
+
+
+    }
+
+}
