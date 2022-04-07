c99d236d344f0b77dea5bd00c5f52e5fa62c5e0c
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-07 22:02:43 +0900

    객체지향 테스트

--- src/객체지향/FunctionTest.java
+++ src/객체지향/FunctionTest.java
@@ -19,12 +19,25 @@ public class FunctionTest {
         for (i = 0; i <= 100; i++) {
             sum += i;
         }
+
+        return sum;
     }
 
 
 
 
     public static void main(String[] args) {
+        int n1=10;
+        int n2=20;
+
+        int total=addNum(n1,n2);
+        System.out.println(total);
+
+
+        sayHello("안녕하세요");
+
+        total=calcSum();
+        System.out.println(total);
 
     }
 }
