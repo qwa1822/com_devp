699470a23b6f38a7da5263751b16fda69ec8bba0
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-07 23:58:16 +0900

    getter,satter 연습

--- src/toto/Book.java
+++ src/toto/Book.java
@@ -3,7 +3,7 @@ package toto;
 public class Book {
 
     private int price;  //field price
-
+    private String title;
 
     public  int getPrice(){
         return this.price; //this는 내자신 인스턴스를 참조하는 예약어
@@ -11,5 +11,13 @@ public class Book {
 
     public void setPrice(int price){  //지역변수
         this.price=price;
+    }
+
+    public String gettitle(){
+        return this.title;
+    }
+
+    public void Settitle(String title){
+        this.title=title;
     }
 }
--- src/toto/BookExam01.java
+++ src/toto/BookExam01.java
@@ -5,10 +5,14 @@ public class BookExam01 {
         Book b1=new Book();
 
 //        b1.price=100;
-//
+//      setter,getter=프로퍼티(property)
 //        System.out.println(b1.price);
 
         b1.setPrice(500);
         System.out.println(b1.getPrice());
+
+
+        b1.Settitle("수민");
+        System.out.println(b1.gettitle());
     }
 }
