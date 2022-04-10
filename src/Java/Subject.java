62fedfcbf5a98d7bf7e1fb9a35fe622031845019
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-10 19:46:24 +0900

    Student class Exam01~03

---
+++ src/Java/Subject.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class Subject {
+
+    String subjectName;
+    int score;
+    int subjectID;
+
+}
---
+++ src/Java/SubjectTest.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class SubjectTest {
+    public static void main(String[] args) {
+
+        Student studentLee=new Student(100,"Lee");
+        studentLee.setKoreaSubject("국어",100);
+        studentLee.setMathSubject("영어",95);
+
+
+        Student studentKim=new Student(200,"Kim");
+        studentKim.setKoreaSubject("국어",95);
+        studentKim.setMathSubject("영어",90);
+
+
+        studentLee.showScoreInfo();
+        studentKim.showScoreInfo();
+    }
+}
