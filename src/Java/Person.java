32b9c18f79b49d32d8e6bbe9e40dd426b9d8a19d
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-10 19:21:34 +0900

    Person class 생성자연습.

---
+++ src/Java/Person.java
@@ -0,0 +1,28 @@
+package Java;
+
+public class Person {
+
+    public int height;
+    public int weight;
+    public String name;
+    public int old;
+
+
+
+    public Person(int height, int weight, String name, int old){
+        this.height=height;
+        this.weight=weight;
+        this.name=name;
+        this.old=old;
+
+    }
+
+    public void showInfo(){
+        System.out.println("키는"+this.height+"이고,"+"몸무게는"+this.weight+"이고, 이름은"+this.name+"이고 ,그의 나이는"+this.old+"입니다");
+    }
+
+
+
+
+}
+
---
+++ src/Java/PersonTest.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class PersonTest {
+    public static void main(String[] args) {
+
+        Person p1=new Person(180,89,"mr's kim",37);
+
+       p1.showInfo();
+    }
+}
