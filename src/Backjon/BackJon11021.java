81a84463445c8ed454e493d824f3622c62dca48d
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-10 14:25:43 +0900

    A+B 기찍 7

---
+++ src/Backjon/BackJon11021.java
@@ -0,0 +1,29 @@
+package Backjon;
+
+import java.util.Scanner;
+
+public class BackJon11021 {
+    public static void main(String[] args) {
+
+
+
+
+
+        Scanner sc=new Scanner(System.in);
+
+        int i=sc.nextInt();
+
+        for(int a=1; a<=i; a++){
+            int b=sc.nextInt();
+            int d=sc.nextInt();
+
+            System.out.println("Case #"+a + ":"+(b+d));
+
+    }
+
+        sc.close();
+
+
+
+    }
+}
