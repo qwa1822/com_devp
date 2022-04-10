722d1960adb807411db219ae96ad115ee78ce4bd
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-10 14:06:10 +0900

    빠른 A+b

--- src/Backjon/Ex02.java
+++ src/Backjon/Ex02.java
@@ -1,15 +1,38 @@
 package Backjon;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
 public class Ex02 {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        in.close();
 
-        for(int i = 1; i<10;i++) {
-            System.out.println(a+" * "+i+" = "+(a*i));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
         }
+        br.close();
+
+        bw.flush();
+        bw.close();
+
     }
 }
+
+
+
+
+
+        }
+    }
