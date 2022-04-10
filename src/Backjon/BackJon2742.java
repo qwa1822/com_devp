a69b619924f74ea36792116a6df9a3f4a9be9003
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-10 14:16:37 +0900

    기찍 N

---
+++ src/Backjon/BackJon2742.java
@@ -0,0 +1,19 @@
+package Backjon;
+
+import java.util.Scanner;
+
+public class BackJon2742 {
+    public static void main(String[] args) {
+
+
+        Scanner sc=new Scanner(System.in);
+
+        int i=sc.nextInt();
+
+
+        while(i>0){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
