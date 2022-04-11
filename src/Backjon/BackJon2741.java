4874d52d38775417b1e1f5032d5db959395e0fce
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-11 16:13:19 +0900

    BackJon2741

--- src/Backjon/BackJon2741.java
+++ src/Backjon/BackJon2741.java
@@ -7,9 +7,9 @@ public class BackJon2741 {
 
 
         Scanner sc=new Scanner(System.in);
-
         int a=sc.nextInt();
-        for(int i=1 ;i<=a; i++){
+
+        for(int i=0; i<=a; i--){
             System.out.println(i);
         }
     }
