a37bec2ec920807a639e4d2716abd2d9a6c10bb8
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-12 23:21:41 +0900

    Abstract

---
+++ src/toto/Controller.java
@@ -0,0 +1,17 @@
+package toto;
+
+/*
+Controller의 종류가 여러개더라.
+초기화 - 같은코드
+실행  - 다른 코드
+마무리 - 같은 코드
+
+ */
+public abstract class Controller {
+    public void execute(){
+        //초기화
+        //실행
+        //마무리
+
+    }
+}
