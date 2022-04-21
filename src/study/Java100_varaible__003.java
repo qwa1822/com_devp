10efe51961860e2668a41072591e9c86da111784
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-21 18:01:18 +0900

    Variable 문자열,int,string,자릿수 구하기

---
+++ src/study/Java100_varaible__003.java
@@ -0,0 +1,26 @@
+package study;
+
+public class Java100_varaible__003 {
+    public static void main(String[] args) {
+        //2            //2
+        //byte,short,int,long,char
+
+
+        int a = 12345;
+
+
+        String str = "12345";
+
+        System.out.println(str.length());
+
+        String stra = String.valueOf(a);
+
+        System.out.println(stra.length());
+
+
+        /*
+         * 문자열+정수형=?*/
+        System.out.println(12345+1);   //12346
+        System.out.println(str+1);  // 12345+1=123451
+    }
+}
