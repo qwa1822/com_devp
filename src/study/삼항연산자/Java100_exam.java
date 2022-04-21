6329636f5f0ccb4ccca20594f5e4996bba0c29ab
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-21 21:33:48 +0900

    대문자 A~Z 출력

--- src/study/삼항연산자/Java100_exam.java
+++ src/study/삼항연산자/Java100_exam.java
@@ -3,11 +3,11 @@ package study.삼항연산자;
 public class Java100_exam {
     public static void main(String[] args) {
 
-    //65는 ASCII 코드 A
+    //65는 ASCII 코드 A는 65 Z 는 90
         //90 Z <<
        for(int i=65; i<=90; i++){
 
-    //char을붙여서 문자로변환
+    //char을붙여서 문자로변환                    A ~Z
            System.out.print((char)i+" "); // 65 66 67 68 69 70..........90
        }
 
