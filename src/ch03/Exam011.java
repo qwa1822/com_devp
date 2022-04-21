93cab822df52cb688317ac7df7af96ae75299744
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-21 13:15:08 +0900

    String 클래스 메서드이용.

---
+++ src/ch03/Exam011.java
@@ -0,0 +1,21 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class Exam011 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc=new Scanner(System.in);
+
+
+
+        String aa=sc.nextLine();
+        int s=sc.nextInt();
+
+        for(int i=0; i<s; i++){
+            System.out.print(aa);
+        }
+    }
+}
---
+++ src/cju/String/Exam.java
@@ -0,0 +1,13 @@
+package cju.String;
+
+import java.util.Scanner;
+
+public class Exam {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        String s=sc.nextLine();
+
+    }
+}
