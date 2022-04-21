74f05f75c92d22439cdfb2392e74af4cb0c37a11
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-21 17:42:13 +0900

    String 클래스 메서드이용.

---
+++ src/study/variable.java
@@ -0,0 +1,12 @@
+package study;
+
+public class variable {
+    public static void main(String[] args) {
+//    접근제한자 static 반환타입 메서드이름(파라미터){
+    //구현할 코드 작성
+        //(1) 접근제한자 public , private,proctected,default
+        //public(공공의) ,   private(직원전용) 이런식으로외움
+
+        System.out.println("hello~world");
+}
+}
