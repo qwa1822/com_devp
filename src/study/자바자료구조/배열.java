47302a5b762f792fc2ff6981c897b61ffbd009c8
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-04-24 01:03:03 +0900

    배열공부

---
+++ src/study/자바자료구조/배열.java
@@ -0,0 +1,19 @@
+package study.자바자료구조;
+
+import java.util.Scanner;
+
+public class 배열 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        //배열을 선언하는데 이것을 사용자로부터 입력받아서 그크기의
+        //배열을 부여받음.
+        int []array=new int[n];
+
+
+    }
+}
