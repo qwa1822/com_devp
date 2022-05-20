12e1725cc8d668fe0c40c2fb9855e38536f151b2
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-05-20 19:54:35 +0900

    입출력파일 오름차순 (이름,번호)

---
+++ src/cju/공부/인프런자료구조/문자열입력/문자열출력.java
@@ -0,0 +1,7 @@
+package cju.공부.인프런자료구조.문자열입력;
+
+public class 문자열출력 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/cju/공부/인프런자료구조/자바기능/TwoArray소수찾기.java
@@ -0,0 +1,43 @@
+package cju.공부.인프런자료구조.자바기능;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TwoArray소수찾기 {
+
+//    static int n;
+//    static int [][]grid;
+//    public static void main(String[] args) {
+//
+//        Scanner sc=new Scanner(System.in);
+//        try{
+//            Scanner inFile=new Scanner(new File("data.txt"));
+//            n=inFile.nextInt();
+//            grid=new int[n][n];
+//
+//            for(int i=0; i<n; i++)
+//                for(int j=0; j<n; j++)
+//                    grid[i][j]=inFile.nextInt();
+//            inFile.close();
+//
+//
+//
+//        for(int x=0; x<n; x++){
+//            for(int y=0; y<n; y++){
+//                for(int dir=0; dir<8; dir++){
+//                    for(int len=1; len<=n; len++){
+//                        int val=computeValue(x,y,dir,len);
+//                        if(val!=1 && isPrime(val))
+//                            System.out.println(val);
+//                    }
+//                }
+//            }
+//        }
+//
+//        static int [] offsetX={0,1,1,1,0,-1,-1,-1};
+//        static int [] offsetY={-1,-1,0,1,1,1,0,-1};
+//
+
+    }
+
